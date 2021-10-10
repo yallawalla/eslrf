49da6adbd3975a874497d43dadf942a15d117e12
yallawalla <mr.mocnik@gmail.com>
2021-10-10 22:26:25 +0200

    ...

--- Core/Src/test.c
+++ Core/Src/test.c
@@ -15,9 +15,9 @@ typedef enum { SECOND, FIRST } echo;
 
 extern		TIM_HandleTypeDef htim2,htim3;
 
-_buffer		*icbuf1,*icbuf2;
-uint32_t 	timeout,nBaud;
-uint16_t	*pwmbuf;
+_buffer		*icbuf1,*icbuf2;				// dma buffer ic1,ic2,tim2
+uint16_t	*pwmbuf;								// dma buffer tim3,pwm
+uint32_t 	idle,nBaud;
 
 static void __rearmDMA(uint32_t len)	{																																											\
 	do {																																																				\
@@ -41,7 +41,7 @@ __packed struct callW {
 } callW = {1,0,ON,1,0,NORMAL,8,0,0};
 
 
-__packed struct  a482 {
+__packed struct a482 {
 	uint8_t addr:3, spare1:1, commfail:1, id:1, update:1, spare2:1;  
 	uint8_t r100m:4, r1000m:4;
 	uint8_t r10m:4, r10000m:1, r1m:1, submode:2;
@@ -68,7 +68,7 @@ __packed struct a485 {
 	uint8_t	chk;	
 } ;
 
-__packed struct  a486 {
+__packed struct a486 {
 	uint8_t addr:3, spare1:1, commfail:1, id:1, update:1, spare2:1;  
 	uint8_t SwChk:1,RAM:1,EPROM:1,FIFO:1,Sim:1,To:1,Qsw:1,HV:1;
 	uint8_t BITinproc:1,spare3:6;
@@ -113,29 +113,25 @@ static void			sendCallW(void) {
 * Output				:
 * Return				:
 *******************************************************************************/
-static int32_t getIc(uint32_t ic) {
+static int32_t	getIc(uint32_t ic) {
 	static uint32_t to, bit, dat, cnt;
 	int32_t	ret=EOF;
-
-//				_print("%08X\r\n",ic);
 	if (to) {
 		uint32_t n = ((ic - to) + nBaud/2) / nBaud;
 		while (n-- && cnt <= nBits + nStop) {
 			dat = (dat | bit) >> 1;
 			++cnt;
-//			if(bit)
-//				_print("-");
-//			else
-//				_print("_");
+//		bit ? _print("-") : _print("_");
 		}
-		timeout=HAL_GetTick()+5;
+		idle=HAL_GetTick()+5;		
 		bit ^= 1 << (nBits + nStop);
 		if (cnt > nBits + nStop) {
 			ret=dat;
 			dat = cnt = 0;
 		}
-	} else 
+	} else {
 			bit = dat = cnt = 0;
+	}
 	to = ic;
 	return ret;
 }
@@ -146,89 +142,82 @@ static int32_t getIc(uint32_t ic) {
 * Return				:
 *******************************************************************************/
 void	test(void) {
-	_print("ESLRF test\r\n");
 	
-	nBaud=SystemCoreClock/baudrate-1;
 	if(!pwmbuf)	{
+		nBaud=SystemCoreClock/baudrate-1;
+		_print("ESLRF test\r\n");
 		pwmbuf=malloc(1024*sizeof(uint16_t));
 		memcpy(pwmbuf,"\0xffff",sizeof(uint16_t));	//dummy edge, first CCR1 !!!
 		htim3.Instance->ARR=nBaud;
 		HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_1);
 		HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_3);
 		HAL_TIM_DMABurst_WriteStart(&htim3,TIM_DMABASE_CCR1,TIM_DMA_UPDATE,(uint32_t *)pwmbuf,TIM_DMABURSTLENGTH_3TRANSFERS);
-	}
-	if(!icbuf1)	{
 		icbuf1=_buffer_init(1024);
 		HAL_TIM_IC_Start_DMA(&htim2,TIM_CHANNEL_1, (uint32_t *)icbuf1->_buf, icbuf1->size/sizeof(uint32_t));
-	}
-	if(!icbuf2)	{
 		icbuf2=_buffer_init(1024);
 		HAL_TIM_IC_Start_DMA(&htim2,TIM_CHANNEL_2,  (uint32_t *)icbuf2->_buf, icbuf2->size/sizeof(uint32_t));
 	}
+	switch (getchar()) {
+		case __F1:
+			callW.opmode=ON;
+			callW.submode=NORMAL;
+			sendCallW();
+			break;
+		case __F2:
+			callW.opmode=ON;
+			callW.submode=VERSION;
+			sendCallW();
+			break;
+		case __F3:
+			callW.opmode=ON;
+			callW.submode=COMM;
+			sendCallW();
+			break;
+		case __F4:
+			callW.opmode=ON;
+			callW.submode=COUNT;
+			sendCallW();
+			break;
+		case __F5:
+			callW.opmode=BIT;
+			callW.submode=NORMAL;
+			sendCallW();
+			break;
+		case __F12:
+			callW.opmode=FIRE;
+			callW.submode=NORMAL;
+			sendCallW();
+			break;
+		case __CtrlY:
+			HAL_NVIC_SystemReset();
+		case EOF:
+		break;
+		default:
+		break;
+	}
 	
+	if(idle && HAL_GetTick() > idle) {
+		idle=0;
+		_buffer_put(icbuf1,&idle,sizeof(uint32_t));
+		_buffer_put(icbuf2,&idle,sizeof(uint32_t));
+	}
 
-	while(1) {
-		switch (getchar()) {
-			case __Esc:
-				_print("exit...  "); 		
-				return;
-			case '1':
-			case __F1:
-				callW.opmode=ON;
-				callW.submode=NORMAL;
-				sendCallW();
-				break;
-			case __F2:
-				callW.opmode=ON;
-				callW.submode=VERSION;
-				sendCallW();
-				break;
-			case __F3:
-				callW.opmode=ON;
-				callW.submode=COMM;
-				sendCallW();
-				break;
-			case __F4:
-				callW.opmode=ON;
-				callW.submode=COUNT;
-				sendCallW();
-				break;
-			case __F5:
-				callW.opmode=BIT;
-				callW.submode=NORMAL;
-				sendCallW();
-				break;
-			case __F12:
-				callW.opmode=FIRE;
-				callW.submode=NORMAL;
-				sendCallW();
-				break;
-			case __CtrlY:
-				HAL_NVIC_SystemReset();
-			case EOF:
-			break;
-			default:
-			break;
-		}
-		
-		if(timeout && HAL_GetTick() > timeout) {
-			timeout=0;
-			_buffer_put(icbuf1,&timeout,sizeof(uint32_t));
-			_buffer_put(icbuf2,&timeout,sizeof(uint32_t));
-		}
+	icbuf1->_push = &icbuf1->_buf[(icbuf1->size - htim2.hdma[TIM_DMA_ID_CC1]->Instance->NDTR*sizeof(uint32_t))];
+	icbuf2->_push = &icbuf2->_buf[(icbuf2->size - htim2.hdma[TIM_DMA_ID_CC2]->Instance->NDTR*sizeof(uint32_t))];
 
-		icbuf1->_push = &icbuf1->_buf[(icbuf1->size - htim2.hdma[TIM_DMA_ID_CC1]->Instance->NDTR*sizeof(uint32_t))];
-		icbuf2->_push = &icbuf2->_buf[(icbuf2->size - htim2.hdma[TIM_DMA_ID_CC2]->Instance->NDTR*sizeof(uint32_t))];
-		for (int32_t ch=0; _buffer_pull(icbuf1,&ch,sizeof(uint32_t));) {
-			ch=getIc(ch);
+	if(_buffer_count(icbuf1) && _buffer_count(icbuf2)) {
+		uint32_t	t1,t2;
+		_buffer_pull(icbuf1,&t1,sizeof(uint32_t));
+		_buffer_pull(icbuf2,&t2,sizeof(uint32_t));
+		if(t1-t2 < 72) {
+			int32_t ch=getIc(t1);
 			if(ch != EOF) {
 				if(ch & 0x100)
 					_print("\r\n%02X ",ch & 0xff);
 				else
 					_print("%02X ",ch & 0xff);
-			} else break;
-		}	
-		_wait(2);
-		
-	} 
-}
+			}
+		}
+	}
+} 
+
